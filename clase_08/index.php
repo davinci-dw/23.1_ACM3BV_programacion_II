2dd759152baf27e5d130670b2f67e04e426af4d2
Sergio Medina <hola@sergiomedina.io>
2023-04-10 12:20:56 -0300

    feat: add class example

---
+++ clase_08/index.php
@@ -0,0 +1,37 @@
+<?php
+// (void) -> string
+
+function generateContent() {
+    $exite_funcion = false;
+
+    if($exite_funcion) {
+        function bar() {
+            echo "bar";
+        }
+    } else {
+        function bar() {
+            echo "bar2";
+        }
+    }
+    return "Hello World!";
+}
+
+
+?>
+
+<html>
+    <head>
+        <title>Clase 08</title>
+    </head>
+    <body>
+        <!--?= generateContent() ?-->
+    </body>
+</html>
+
+<?php
+generateContent();
+bar(); //error
+echo "hola";
+//if($exite_funcion) bar();
+//echo generateContent();
+?>
