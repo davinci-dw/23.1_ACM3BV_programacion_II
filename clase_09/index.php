225369367c48dd8b5e9459c44c77a9a18e81d67a
Sergio Medina <hola@sergiomedina.io>
2023-04-17 11:54:48 -0300

    refactor: get content from function

--- clase_09/index.php
+++ clase_09/index.php
@@ -1,32 +1,34 @@
 <?php
 // (void) -> string
+function getContent() {
+    return [
+        [
+            "nombre" => "Producto 1",
+            "precio" => 100,
+            "imagen" => "https://picsum.photos/200/200",
+            "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
+        ],
+        [
+            "nombre" => "Producto 2",
+            "precio" => 200,
+            "imagen" => "https://picsum.photos/200/200",
+            "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
+        ],
+        [
+            "nombre" => "Producto 3",
+            "precio" => 300,
+            "imagen" => "https://picsum.photos/200/200",
+            "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
+        ],
+        [
+            "nombre" => "Producto 4",
+            "precio" => 400,
+            "imagen" => "https://picsum.photos/200/200",
+            "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
+        ]
 
-$misProductos = [
-    [
-        "nombre" => "Producto 1",
-        "precio" => 100,
-        "imagen" => "https://picsum.photos/200/200",
-        "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
-    ],
-    [
-        "nombre" => "Producto 2",
-        "precio" => 200,
-        "imagen" => "https://picsum.photos/200/200",
-        "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
-    ],
-    [
-        "nombre" => "Producto 3",
-        "precio" => 300,
-        "imagen" => "https://picsum.photos/200/200",
-        "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
-    ],
-    [
-        "nombre" => "Producto 4",
-        "precio" => 400,
-        "imagen" => "https://picsum.photos/200/200",
-        "descripcion" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies, nunc nisl aliquet nisl, eget aliquet nisl ni"
-    ]
-];
+    ];
+}
 
 function generateContent($list): string {
     //return json_encode($list);
@@ -50,6 +52,6 @@ function generateContent($list): string {
         <title>Clase 09</title>
     </head>
     <body>
-        <?= generateContent($misProductos) ?>
+        <?= generateContent(getContent()) ?>
     </body>
 </html>
